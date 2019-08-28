2643861307076d3e1d95c4a69d27828ed6c80443
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-08-27 21:47:40 -0400

    Drop the -swift suffix from package name

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "inputsAPI-swift",
+    name: "inputsAPI",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "inputsAPI-swift",
+            name: "inputsAPI",
             targets: ["inputsAPI-swift"]),
     ],
     dependencies: [
