ea67214975d7649866453502358de2ffe9296d84
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-08-27 22:07:10 -0400

    Clean up naming and make lib name same as desired package name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "InputsAPI",
-            targets: ["inputsAPI-swift"]),
+            targets: ["InputsAPI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "inputsAPI-swift",
-            dependencies: ["Promise"]),
+            name: "InputsAPI",
+            dependencies: ["Promise"],
+            path: "Sources/inputsAPI"
+        ),
         .testTarget(
             name: "inputsAPI-swiftTests",
-            dependencies: ["inputsAPI-swift"]),
+            dependencies: ["InputsAPI"]),
     ]
 )
